86e4116d1f6afe29f38cd0f047949dfdbb0c0a54
SazzDS <sazz000009@gmail.com>
2019-12-23 18:11:21 +0530

    coffiee-shop

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-import "../../public/plugins/bootstrap/css/bootstrap.min.css"
+import "../plugins/bootstrap/css/bootstrap.min.css"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
