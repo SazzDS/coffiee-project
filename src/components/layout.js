0194815ceb4fde133c1575cbe30082cfef96f708
SazzDS <sazz000009@gmail.com>
2019-12-25 20:35:35 +0530

    coffiee-project

--- src/components/layout.js
+++ src/components/layout.js
@@ -5,15 +5,18 @@
  * See: https://www.gatsbyjs.org/docs/use-static-query/
  */
 
-import React from "react"
-import PropTypes from "prop-types"
+import React from "react";
+import PropTypes from "prop-types";
+import {StaticQuery, graphql} from "gatsby";
 
 // import Header from "./header"
-import "./layout.css"
+import "./layout.css";
+import Navbar from "./Globals/Navbar";
 
 const Layout = ({ children }) => (
     <>
-      {children}
+	    <Navbar />
+		{children}
     </>
   )
 
